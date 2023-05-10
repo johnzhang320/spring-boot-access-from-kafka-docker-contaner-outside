c10cef961e9187b68123a174345fbc53842637fa
johnzhang320 <johnzhang320@gmail.com>
2023-05-09 18:54:15 -0700

    Finalize

--- src/main/java/com/configure/kafka/producer/consumer/config/Constants.java
+++ src/main/java/com/configure/kafka/producer/consumer/config/Constants.java
@@ -5,8 +5,8 @@ public interface Constants {
 
     public final String GROUP_ID_CONFIG="myGroup";
 
-    public final String BOOTSTRAP_NAME="localhost:29092";
+    // public final String BOOTSTRAP_NAME="localhost:29092";
 
-    //public final String BOOTSTRAP_NAME="ec2-52-37-52-208.us-west-2.compute.amazonaws.com:29092";
+    public final String BOOTSTRAP_NAME="ec2-A-B-C-D.us-west-2.compute.amazonaws.com:29092";
 
 }
